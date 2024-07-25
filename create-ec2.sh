7c84f3ff5b764516ece5eeeeeb4b7fdecadf57e1
Mani Kanta <konugantimanikantareddy@gmail.com>
2024-07-25 08:13:47 +0530

    Added some text

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-
+domain_name="dawsmani.site"
+hosted_zone_id="Z0518893283P6UJCL06B2"
 
 for name in ${instances[@]}; do
     if [ $name == "shipping" ] || [ $name == "mysql" ]
@@ -11,5 +12,42 @@ for name in ${instances[@]}; do
         instance_type="t3.micro"
     fi
     echo "Creating Instances for: $name with instance type: $instance_type"
+    
+    instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type --security-group-ids sg-00fdfc2b0e8a3e6e9 --subnet-id subnet-065df3d78ec2dccbf --query 'Instances[0].InstanceId' --output text)
 
+    echo "Instance created for: $name"
+
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+
+    if [ $name == "web" ]
+    then
+        aws ec2 wait instance-running --instance-ids $instance_id # We need to wait untill it is up & running
+        public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
+        ip_to_use=$public_ip
+    else
+        private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+        ip_to_use=$private_ip
+    fi
+    
+    echo "creating R53 record for $name"
+    aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
+    {
+        "Comment": "Creating a record set for '$name'"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$name.$domain_name'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$ip_to_use'"
+            }]
+        }
+        }]
+    }'
 done
+
+
+
+
+
