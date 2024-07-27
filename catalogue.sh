0875c8fc1edd8a1b087ea451a3fa4228eaa1028c
Mani Kanta <konugantimanikantareddy@gmail.com>
2024-07-27 12:05:17 +0530

    Added some text

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,7 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
+    echo -e "$R Please run this script with root access. $N"
     exit 1 # manually exit if error comes.
 else
     echo "You are super user."
--- mongodb.sh
+++ mongodb.sh
@@ -21,7 +21,7 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this script with root access."
+    echo -e "$R Please run this script with root access. $N"
     exit 1 # manually exit if error comes.
 else
     echo "You are super user."
